29b9ab22cb51e9fd05e60e5822d01b7460eec9a2
Levi Roberts <bugs181@gmail.com>
2021-03-20 04:52:09 -0500

    Added ability to extend Flow using modules/functions
    - Flow can now easily be extended using Functions or modules (or class instances).
    - The lookup times for modules are O(2) using ES6 Proxy syntax to allow shortened codability.
    - Modules are available as a static property and instance property to keep beginners from struggling with syntax issues.
    - Added a Filter example to demonstrate how these modules work
    - TODO: Still need to export modules without CommonJS syntax (perhaps browserify them)
    - TODO: Still need to invert the arguments for next pipe.

---
+++ examples/each-iterator.js
@@ -0,0 +1,42 @@
+const Flow = require('../build/flow')
+Flow.use('Filter', require('../libs/Filter'))
+
+const animals = [
+  { species: 'cat', name: 'fluffy' },
+  { species: 'dog', name: 'woofie' },
+  { species: 'cow', name: 'barney' },
+  { species: 'zebra', name: 'spot' },
+  { species: 'zebra', name: 'dot' },
+]
+
+function logResult(description, result) {
+  console.log(description)
+  console.log(result)
+  console.log()
+}
+
+let flow
+
+// Find dog named woolfie
+flow = new Flow()
+flow
+  .from(async () => animals)
+  .to(flow.Filter, 'name', 'woofie' ) // Filter using a single key/value
+  .to(logResult, 'Find dog named woolfie')
+  .run()
+
+// Find all zebras
+flow = new Flow()
+flow
+  .from(async () => animals)
+  .to(flow.Filter, { species: 'zebra' }) // Filter more complex objects
+  .to(logResult, 'Find all zebras')
+  .run()
+
+// spot the zebra
+flow = new Flow()
+flow
+  .from(async () => animals)
+  .to(flow.Filter, { species: 'zebra', name: 'spot' }) // Filter more complex objects (more specifically; narrowing our results)
+  .to(logResult, 'spot the zebra')
+  .run()
---
+++ libs/Filter.js
@@ -0,0 +1,34 @@
+async function Filter(str, value, arr) {
+  // Sort by object keys/values instead
+  if (typeof str === 'object') {
+    const obj = str
+    arr = value
+    const results = []
+
+    for (const item of arr) {
+
+      // All or nothing key/value match
+      let itemMatches = false
+      for (const key of Object.keys(obj)) {
+        if (!item[key]) {
+          itemMatches = false
+          continue
+        }
+
+        if (item[key] === str[key])
+          itemMatches = true
+        else
+          itemMatches = false
+      }
+
+      if (itemMatches)
+        results.push(item)
+    }
+
+    return results
+  } else {
+    return arr.filter(obj => obj[str] === value)
+  }
+}
+
+module.exports = Filter
